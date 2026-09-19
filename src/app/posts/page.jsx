823d95a95d64a77890d52854277247d16a16a324
hasantanvir357-svg <hasantanvir357@gmail.com>
2026-09-19 10:52:03 +0600

    Fetch and display posts from API on PostPage

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const PostPage = () => {
+const PostPage = async () => {
+
+    const res= await fetch('https://jsonplaceholder.typicode.com/posts')
+    const posts = await res.json()
+
+
     return (
         <div>
-            <h2>Mentorship</h2>
+            <h2>Hello Posts: {posts.length}</h2>
         </div>
     );
 };
